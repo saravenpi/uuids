6713da7829eb0ee8bb69f1c5456d4da31c737520
saravenpi <saravenpi@yandex.com>
2021-02-21 23:17:47 +0100

    commenting the code

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 module.exports.geni = function geni(secret) {
-    if (!secret) {
-      var secret = 123
-    }
-    var time = new Date().getTime();
-    return secret * time
+  if (!secret) { //verify if there is a secret
+    var secret = 123 //else we asign a default one
+  }
+  var time = new Date().getTime(); //we generate from the time
+  return secret * time //and multiplying with the time so we can't deduce a date from and id
 }
 
 module.exports.gens = function gens(secret) {
-  if (!secret) {
-    var secret = 123
+  if (!secret) { //verify if there is a secret
+    var secret = 123 //else we asign a default one
   }
-  var i =  secret * new Date().getTime();
-  var a = i.toString().split('');
-  var f = ''
-  for (var i = 0; i < a.length; i++) {
-    c='abcdefghijklmnopqrstuvwxyz'.charAt(Number(a[i]));
-    f += c
+  var i = secret * new Date().getTime(); //we generate from the time and multiply it to the secret so we can't deduce a date from and id
+  var a = i.toString().split(''); //we convert the number to an array of numbers
+  var f = '' //we create a empty string to modify for later
+  for (var i = 0; i < a.length; i++) { //loop to convert the id to a string from the alphabet position of the number
+    c = 'abcdefghijklmnopqrstuvwxyz'.charAt(Number(a[i])); //we create a value associated to the letter
+    f += c //we add the letter to the string
   }
-  return f
+  return f //we return the final string
 }
