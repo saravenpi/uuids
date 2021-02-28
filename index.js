bff20d8dbfa90cc58d06c7073a9bebc7e974eae9
saravenpi <saravenpi@protonmail.com>
2021-02-28 20:05:34 +0100

    update: removing useless syntax

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+var char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 module.exports.geni = function geni(secret) {
   if (!secret) { //verify if there is a secret option
-    var secret = 123 //else we asign a default one
+    secret = 123 //else we asign a default one
   }
   if (typeof secret === 'string' || secret instanceof String) { //if the secret is a string
     var a = secret.split(''); //we convert the string to an array of letters
     secret = 1 //reset the secret variable
     for (var i = 0; i < a.length; i++) { //we'll generate the new secret from the string
-      if (alphabet.includes(a[i])) {
-        c = alphabet.indexOf(a[i]) //we take the corresponding number for each letter
+      if (char.includes(a[i])) {
+        c = char.indexOf(a[i]) //we take the corresponding number for each letter
         secret = secret * c //we add it to the new secret in int type
-      } else { //if not in our alphabet
+      } else { //if not in our char
         console.log("not in string:", a[i])
         secret = secret * 1 //we leave it like this
       }
@@ -22,16 +22,16 @@ module.exports.geni = function geni(secret) {
 
 module.exports.gens = function gens(secret) {
   if (!secret) { //verify if there is a secret option
-    var secret = 123 //else we asign a default one
+    secret = 123 //else we asign a default one
   }
   if (typeof secret === 'string' || secret instanceof String) { //if the secret is a string
     var a = secret.split(''); //we convert the string to an array of letters
     secret = 1 //reset the secret variable
     for (var i = 0; i < a.length; i++) { //we'll generate the new secret from the string
-      if (alphabet.includes(a[i])) {
-        c = alphabet.indexOf(a[i]) //we take the corresponding number for each letter
+      if (char.includes(a[i])) {
+        c = char.indexOf(a[i]) //we take the corresponding number for each letter
         secret = secret * c //we add it to the new secret in int type
-      } else { //if not in our alphabet
+      } else { //if not in our char
         console.log("not in string:", a[i])
         secret = secret * 1 //we leave it like this
       }
@@ -40,8 +40,8 @@ module.exports.gens = function gens(secret) {
   var i = secret * new Date().getTime(); //we generate from the time and multiply it to the secret so we can't deduce a date from and id
   var a = i.toString().split(''); //we convert the number to an array of numbers
   var f = '' //we create a empty string to modify for later
-  for (var i = 0; i < a.length; i++) { //loop to convert the id to a string from the alphabet position of the number
-    c = alphabet.charAt(Number(a[i])); //we create a value associated to the letter
+  for (var i = 0; i < a.length; i++) { //loop to convert the id to a string from the char position of the number
+    c = char.charAt(Number(a[i])); //we create a value associated to the letter
     f += c //we add the letter to the string
   }
   return f //we return the final string
